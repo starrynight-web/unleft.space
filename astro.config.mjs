b7e4f30e45c05d367ab9d2bb71223bbd4db4244e
Mahtabul Al Nahian <mahtabulalnahian@gmail.com>
2026-03-31 00:01:14 +0600

    feat: migrate to Vercel adapter and integrate Vercel Analytics and Speed Insights

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: vercel(),
   site: "https://www.unleft.space",
   integrations: [
     react(),
